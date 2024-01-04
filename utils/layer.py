b1704098df65fa535a3dc7ea005a630f7f673686
kyle1james <lilaresearch@gmail.com>
2024-01-04 11:16:18 -0500

    basic feedforward

--- utils/layer.py
+++ utils/layer.py
@@ -15,16 +15,20 @@ class Layer:
         self.layer: int = 0
         self.neurons: dict = dict()
 
-    def create_neurons(self, group_size: int = 3):
+    def create_neurons(self, group_size: int = 3, random_activation=False):
         '''
         Initialzes neurons in circular fashion
-        2x3
+        2x3s
         '''
         n = len(self.input)
+        p_of_x = True  # TODO update to allow for neurons with diverse activations
+        if random_activation:
+            p_of_x = np.random.choice([0, 1])
         for i in range(n):
             # circular array
             delta_group = [self.input[(i + j) % n] for j in range(group_size)]
-            self.neurons[i] = Neuron(inputs=delta_group, layer=self.layer)
+            self.neurons[i] = Neuron(
+                inputs=delta_group, layer=self.layer, tanh=p_of_x)
 
     def feed_forward(self):
         '''
--- utils/neuron.py
+++ utils/neuron.py
@@ -19,7 +19,7 @@ class Neuron:
         metrics (Dict[str, float]): A dictionary for storing various performance metrics.
     """
 
-    def __init__(self, inputs: np.array(np.array), layer: int) -> None:
+    def __init__(self, inputs: np.array(np.array), layer: int, tanh=True) -> None:
         """Initialize the Neuron with random weights, bias, and specified dimensions and layer.
 
         Args:
@@ -31,7 +31,7 @@ class Neuron:
         self.edges = list()
         self.layer = layer
         self.metrics = {}  # Empty dictionary for metrics
-        self.tanh = True
+        self.tanh = tanh
         self.delta: np.array = None  # activation function output
 
     def activate(self, tanh=False) -> np.ndarray:
@@ -39,13 +39,13 @@ class Neuron:
 
         Args:
             inputs (np.ndarray): The inputs to the neuron.
-            where I = 2x3 matrix. cols: (input, output), rows: examples
+            where I = 3x2 matrix. cols: (input, output), rows: examples
 
         Returns:
             np.ndarray: The output of the neuron after applying the weights, bias, and activation function.
         """
         self.tanh = tanh
-        # Simple linear activation: weights * inputs + bias
+        # Simple linear/non-linear activation: weights * inputs + bias
 
         delta = (np.dot(self.inputs, self.weights.T) + self.bias).T
         if self.tanh:
@@ -61,7 +61,7 @@ class Neuron:
         '''
         print(self.tanh)
         if self.tanh:
-            return (1.0 - np.tanh(self.inputs) ** 2).T
+            return (1.0 - np.tanh(self.delta) ** 2)
         return
 
     @staticmethod
