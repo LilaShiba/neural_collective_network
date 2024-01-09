fb507bd0da74bf5abe15d99c6f73f6fcb974fcdf
kyle1james <lilaresearch@gmail.com>
2024-01-09 12:32:40 -0500

    subprocesses 1-2 created & tested

--- main.py
+++ main.py
@@ -15,52 +15,24 @@ if __name__ == "__main__":
     # Compute the y values using a non-linear function (e.g., sine)
     y_values = np.sin(x_values)
     # Combine x and y into a nested list of tuples
-    dataset = list(zip(x_values, y_values))
-    plt.plot(dataset)
-    plt.title('Input')
-    plt.show()
+    dataset = np.array(list(zip(x_values, y_values)))
+    # plt.plot(dataset[:, 0], dataset[:, 1])
+    # plt.title('Input')
+    # plt.show()
 
-    # Subprocess 1: Create, train, view
     # Input Layer
     input_layer = Layer(input=dataset)
-    input_layer.create_neurons()
-    input_layer.train()
-    input_layer.see_loss_grad()
-
-    # Subprocess 2:
-    # extract weights and input for neuron transfer
-    input_layer_weights = [n.weights for n in input_layer.neurons.values()]
-    # Hidden Layer
-    n = len(input_layer_weights)//2
-    # Pair up the elements from the two lists
-    paired_data = list(zip(input_layer_weights, dataset))
-    # Randomly select half of the pairs
-    selected_pairs = random.sample(paired_data, n // 2)
-    # Separate the pairs back into two lists
-    half_weights, input_half = zip(*selected_pairs)
-
-    # Subprocess 1:
+    # Subprocess 1: Create, train, view
+    input_layer_weights = input_layer.cycle()
+    # Subprocess 2: select and transfer weights, input
+    half_weights, input_half = input_layer.pass_data()
+    # Hidden Layer: Subprocess 1
     h_one = Layer(input=input_half, weights=half_weights)
-    h_one.create_neurons()
-    h_one.train()
-    h_one.see_loss_grad()
-
+    input_layer_weights = h_one.cycle(see_graph=True)
     # Subprocess 2:
-    input_layer_weights = [n.weights for n in h_one.neurons.values()]
-    # Hidden Layer
-    n = len(input_layer_weights)//2
-    # Pair up the elements from the two lists
-    paired_data = list(zip(input_layer_weights, dataset))
-    # Randomly select half of the pairs
-    selected_pairs = random.sample(paired_data, n // 2)
-    # Separate the pairs back into two lists
-    half_weights, input_half = zip(*selected_pairs)
+    half_weights, input_half = h_one.pass_data()
     out_put_layer = Layer(input=input_half, weights=half_weights)
-
-    # Subprocess 1:
-    out_put_layer.create_neurons()
-    out_put_layer.train()
-    out_put_layer.see_loss_grad()
-
+    # Output Layer: Subprocess
+    output_layer_weights = out_put_layer.cycle(see_graph=True)
     # Subprocess 3:
     # Prediction
